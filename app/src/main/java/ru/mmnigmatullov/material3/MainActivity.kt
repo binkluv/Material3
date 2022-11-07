d37734b0fa54e354edc0ba2ac265ab1a6153ad11
Marsel <42341360+binkluv@users.noreply.github.com>
2022-11-07 17:52:38 +0300

    Material3App-example

--- app/src/main/java/ru/mmnigmatullov/material3/MainActivity.kt
+++ app/src/main/java/ru/mmnigmatullov/material3/MainActivity.kt
@@ -4,40 +4,63 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.mmnigmatullov.material3.ui.theme.Material3Theme
 
+
+@Suppress("DEPRECATION")
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Material3Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    Scaffold(
+                        floatingActionButton = {
+                            FloatingActionButton(onClick = { }) {
+                                Icon(imageVector = Icons.Default.Add,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colorScheme.onPrimaryContainer
+                                    )
+                            }
+                        },
+                        topBar = {
+                            SmallTopAppBar(
+                                title = {
+                                    Text(text = "Material 3")
+                                },
+                                colors = TopAppBarDefaults.smallTopAppBarColors(
+                                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                                    titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                                )
+                            )
+                        }
+                    ) { values ->
+                        LazyColumn(contentPadding = values){
+                            items(20) {
+                                ImageCard(
+                                    title = "Bacon ipsum",
+                                    description = "Bacon ipsum dolor amet biltong salami pork belly andouille, buffalo pork swine pastrami short ribs shankle turkey corned beef. Strip steak shank shoulder kielbasa ball tip. Bacon meatloaf pork loin tenderloin venison kevin drumstick swine tri-tip chuck. Tri-tip meatball flank chislic pork belly, prosciutto jerky doner porchetta filet mignon tail meatloaf. Shank pig tail bacon pork chop chicken, short ribs pork belly ground round pastrami hamburger. Swine cow strip steak flank turducken, jerky ham hock chislic short loin rump tenderloin bresaola pancetta pork. Meatloaf fatback chuck buffalo beef ribs bacon, beef bresaola short ribs andouille short loin spare ribs ham.",
+                                    modifier = Modifier.padding(16.dp)
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Material3Theme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/ru/mmnigmatullov/material3/ui/theme/Theme.kt
+++ app/src/main/java/ru/mmnigmatullov/material3/ui/theme/Theme.kt
@@ -10,31 +10,73 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Violet80,
+    onPrimary = Violet20,
+    primaryContainer = Violet30,
+    onPrimaryContainer = Violet90,
+    inversePrimary = Violet40,
+
+    secondary = DarkViolet80,
+    onSecondary = DarkViolet20,
+    secondaryContainer = DarkViolet30,
+    onSecondaryContainer = DarkViolet90,
+
+    tertiary = Pink80,
+
+    error = Red80,
+    onError = Red20,
+    errorContainer = Red30,
+    onErrorContainer = Red90,
+
+    background = Grey10,
+    onBackground = Grey90,
+
+    surface = GreyViolet30,
+    onSurface = GreyViolet80,
+    inverseSurface = Grey90,
+    inverseOnSurface = Grey10,
+    surfaceVariant = GreyViolet30,
+    onSurfaceVariant = GreyViolet80,
+    outline = GreyViolet80
+
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = Violet40,
     onPrimary = Color.White,
+    primaryContainer = Violet90,
+    onPrimaryContainer = Violet10,
+    inversePrimary = Violet80,
+
+    secondary = DarkViolet40,
     onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    secondaryContainer = DarkViolet90,
+    onSecondaryContainer = DarkViolet10,
+
+    tertiary = Pink80,
+
+    error = Red40,
+    onError = Color.White,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+
+    background = Grey99,
+    onBackground = Grey10,
+
+    surface = GreyViolet90,
+    onSurface = GreyViolet30,
+    inverseSurface = Grey20,
+    inverseOnSurface = Grey95,
+    surfaceVariant = GreyViolet90,
+    onSurfaceVariant = GreyViolet30,
+    outline = GreyViolet50
 )
 
 @Composable
@@ -45,8 +87,8 @@ fun Material3Theme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
+       dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+           val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
         darkTheme -> DarkColorScheme
